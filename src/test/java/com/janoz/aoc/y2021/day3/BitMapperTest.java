26dfbb69c61a8471d30d704093e5e2f67ee0581f
Gijs de Vries <git@janoz.com>
2021-12-03 09:46:00 +0100

    Add annotation

--- src/test/java/com/janoz/aoc/y2021/day3/BitMapperTest.java
+++ src/test/java/com/janoz/aoc/y2021/day3/BitMapperTest.java
@@ -1,10 +1,13 @@
 package com.janoz.aoc.y2021.day3;
 
+import org.junit.jupiter.api.Test;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 class BitMapperTest {
 
+    @Test
     public void testBitmapper() {
         boolean[] actual = new BitMapper().apply("0011");
         assertThat(actual, equalTo(new boolean[]{false,false,true,true}));
