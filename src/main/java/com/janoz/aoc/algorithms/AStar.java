b7d224e6842c523c1911048974dcaa667e3a6cbf
Gijs de Vries <git@janoz.com>
2022-12-12 12:24:54 +0100

    Implement generic AStar

---
+++ src/main/java/com/janoz/aoc/algorithms/AStar.java
@@ -0,0 +1,87 @@
+package com.janoz.aoc.algorithms;
+
+import com.janoz.aoc.collections.AlwaysHashMap;
+import com.janoz.aoc.geo.Point;
+import com.janoz.aoc.geo.Utils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class AStar<NODE> {
+
+    private final HashMap<NODE,Long> distanceMap = new AlwaysHashMap<>(() -> Long.MAX_VALUE);
+    private final BiFunction<NODE, NODE, Boolean> validMovePredicate;
+    private final Function<NODE, NODE[]> neighbourProducer;
+    private final BiFunction<NODE, NODE, Long> distanceCalculator;
+    private final Predicate<NODE> earlyOut;
+    private final Function<NODE, Long> heuristic;
+
+    private AStar(BiFunction<NODE, NODE, Boolean> validMovePredicate, Function<NODE, NODE[]> neighbourProducer, Predicate<NODE> earlyOut, Function<NODE, Long> heuristic) {
+        this.validMovePredicate = validMovePredicate;
+        this.distanceCalculator = (f,t) -> 1L;
+        this.neighbourProducer = neighbourProducer;
+        this.earlyOut = earlyOut;
+        this.heuristic = heuristic;
+    }
+
+    public long calculate(NODE start) {
+        Route<NODE> route = new Route<>(start, 0L, heuristic.apply(start));
+        PriorityQueue<Route<NODE>> heap = new PriorityQueue<>();
+        addRoute(heap, route);
+        while (!heap.isEmpty()) {
+            Route<NODE> source = heap.poll();
+            if (earlyOut.test(source.node)) return source.distance;
+            if (source.distance != distanceMap.get(source.node)) continue; //already found a quicker way
+            Arrays.stream(neighbourProducer.apply(source.node))
+                    .filter(n -> validMovePredicate.apply(source.node,n))
+                    .map(n -> new Step(source,n))
+                    .filter(Step::isPreferable)
+                    .forEach(r -> addRoute(heap,r.to));
+        }
+        return -1;
+    }
+
+    private void addRoute(PriorityQueue<Route<NODE>> heap, Route<NODE> route) {
+        distanceMap.put(route.node, route.distance);
+        heap.add(route);
+    }
+
+    private class Step {
+        Route<NODE> from;
+        Route<NODE> to;
+
+        Step(Route<NODE> from, NODE to) {
+            this.from = from;
+            this.to = new Route<>(to, from.distance + distanceCalculator.apply(from.node, to),heuristic.apply(to));
+        }
+
+        boolean isPreferable() {
+            return to.distance < distanceMap.get(to.node);
+        }
+    }
+
+    private static class Route<NODE> implements Comparable<Route<NODE>> {
+        NODE node;
+        long distance;
+        long expectedDistance;
+
+        @Override
+        public int compareTo(Route o) {
+            return (int)Math.signum(expectedDistance - o.expectedDistance);
+        }
+
+        public Route(NODE p, long distanceUntilNow, long minimumDistanceToTarget) {
+            node = p;
+            this.distance = distanceUntilNow;
+            this.expectedDistance = distanceUntilNow + minimumDistanceToTarget;
+        }
+    }
+
+    public static AStar<Point> for2DGrid(int width, int height, BiFunction<Point,Point, Boolean> validRoutePredicate, Point target) {
+        return new AStar<>(Utils.boundsCheckWrapperForTo(width,height,validRoutePredicate), Point::neighbours, p -> p.equals(target), p -> p.manhattanDistance(target));
+    }
+}
