79d5e159872a34c00dbbeb8c6dfce68e5406b355
Gijs de Vries <git@janoz.com>
2022-12-12 12:22:15 +0100

    Print function for Day 9

---
+++ src/main/java/com/janoz/aoc/geo/BoundingBox.java
@@ -0,0 +1,29 @@
+package com.janoz.aoc.geo;
+
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class BoundingBox {
+    int top,bottom,left,right;
+
+    public BoundingBox(Point origin) {
+        top = origin.y;
+        bottom = origin.y;
+        left= origin.x;
+        right = origin.x;
+    }
+
+    public void addPoint(Point toAdd) {
+        top = Math.min(top,toAdd.y);
+        bottom = Math.max(bottom,toAdd.y);
+        left = Math.min(left,toAdd.x);
+        right = Math.max(right,toAdd.x);
+    }
+
+    public void printGrid(Function<Point,Character> mapToChar) {
+        IntStream.rangeClosed(top,bottom).forEach(y -> {
+            IntStream.rangeClosed(left,right).mapToObj(x -> new Point(x,y)).map(mapToChar).forEach(System.out::print);
+            System.out.println();
+        });
+    }
+}
--- src/main/java/com/janoz/aoc/y2022/day9/Day09.java
+++ src/main/java/com/janoz/aoc/y2022/day9/Day09.java
@@ -1,6 +1,8 @@
 package com.janoz.aoc.y2022.day9;
 
 import com.janoz.aoc.InputProcessor;
+import com.janoz.aoc.StopWatch;
+import com.janoz.aoc.geo.BoundingBox;
 import com.janoz.aoc.geo.Point;
 
 import java.util.Arrays;
@@ -10,21 +12,28 @@ import java.util.Set;
 public class Day09 {
 
     public static void main(String[] args) {
-        String file = "inputs/2022/day09.txt";
+        //String file = "inputs/2022/day09.txt";
+        String file = "inputs/2022/bigsets/aoc_2022_day09_large-2.in";
         Day09 solver = new Day09(10);
+        StopWatch.start();
         InputProcessor.asStream(file, Move::parse).forEach(solver::doMove);
-        System.out.println(solver.visited.size());
+        StopWatch.stopPrint();
+        System.out.println("Part 1: " + solver.visitedPart1.size());
+        System.out.println("Part 2: " + solver.visitedPart2.size());
     }
 
     Day09(int ropeLength) {
         this.ropeLength = ropeLength;
         rope = new Point[ropeLength];
         Arrays.fill(rope, new Point(0,0));
-        visited = new HashSet<>();
-        visited.add(new Point(0,0));
+        visitedPart1 = new HashSet<>();
+        visitedPart1.add(new Point(0,0));
+        visitedPart2 = new HashSet<>();
+        visitedPart2.add(new Point(0,0));
     }
 
-    Set<Point> visited;
+    Set<Point> visitedPart2;
+    Set<Point> visitedPart1;
     Point[] rope;
     int ropeLength;
 
@@ -36,13 +45,32 @@ public class Day09 {
                     rope[i] = moveKnot(rope[i-1], rope[i]);
                 }
             }
-            visited.add(rope[ropeLength-1]);
+            visitedPart1.add(rope[1]);
+            visitedPart2.add(rope[ropeLength-1]);
         }
     }
 
-    static Point moveKnot(Point pulling, Point pulled) {
+    Point moveKnot(Point pulling, Point pulled) {
         int dx = (int)Math.signum(pulling.x - pulled.x);
         int dy = (int)Math.signum(pulling.y - pulled.y);
         return pulled.translate(dx,dy);
+    }
+
+
+    //---------------------//
+    BoundingBox bb = new BoundingBox(new Point(0,0));
+
+    void printGrid() {
+        bb.addPoint(rope[0]);
+        bb.printGrid(p -> {
+            if (p.equals(rope[0])) return 'H';
+            for (int i=1; i<=9; i++) {
+                if (p.equals(rope[i])) return (char)('0' + i);
+            }
+            return visitedPart2.contains(p)?'#':'.';
+        });
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
