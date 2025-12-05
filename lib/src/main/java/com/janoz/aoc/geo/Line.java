25ab9697004880af96393e5b28f1ce43114d3222
Janoz <github@janoz.com>
2025-12-05 06:59:25 +0100

    Removed unused IntRange

--- lib/src/main/java/com/janoz/aoc/geo/Line.java
+++ lib/src/main/java/com/janoz/aoc/geo/Line.java
@@ -1,9 +1,7 @@
 package com.janoz.aoc.geo;
 
-import com.janoz.aoc.collections.IntRange;
 
 import java.util.Objects;
-import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
